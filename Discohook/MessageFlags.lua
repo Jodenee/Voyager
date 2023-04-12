cbc2427bdae7988f67a8dabd50e57d50e2c0e98c
Jodenee <81998397+Jodenee@users.noreply.github.com>
2023-04-12 21:48:31 +0200

    Added MessageFlags object

---
+++ Discohook/MessageFlags.lua
@@ -0,0 +1,22 @@
+local BaseFlags = require(script.Parent.bases.BaseFlags)
+local MessageFlags = {}
+MessageFlags.__index = MessageFlags
+setmetatable(MessageFlags, BaseFlags)
+
+function MessageFlags.fromBitfield(value: number)
+	local self = setmetatable(BaseFlags.fromBitfield(value), MessageFlags)
+	
+	return self
+end
+
+function MessageFlags.fromFlags(flags: {number})
+	local self = setmetatable(BaseFlags.fromBitfield(0), MessageFlags)
+	
+	for _, flag in flags do
+		self.value = bit32.bor(self.value, flag)
+	end
+
+	return self
+end
+
+return MessageFlags
