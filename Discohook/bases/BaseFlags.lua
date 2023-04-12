431f073312143576a33d3f8a3bf73b3f8de3d064
Jodenee <81998397+Jodenee@users.noreply.github.com>
2023-04-12 21:42:53 +0200

    Added bases folder and BaseFlags object

---
+++ Discohook/bases/BaseFlags.lua
@@ -0,0 +1,24 @@
+local BaseFlags = {}
+BaseFlags.__index = BaseFlags
+
+function BaseFlags.fromBitfield(value: number)
+	local self = setmetatable({}, BaseFlags)
+	
+	self.value = value
+	
+	return self
+end
+
+function BaseFlags:hasFlag(flag: number): boolean
+	return bit32.band(self.value, flag) == flag
+end
+
+function BaseFlags:setFlag(flag: number, toggal: boolean): nil
+	if toggal then
+		self.value = bit32.bor(self.value, flag)
+	else
+		self.value = bit32.band(self.value, bit32.bnot(flag))
+	end
+end
+
+return BaseFlags
