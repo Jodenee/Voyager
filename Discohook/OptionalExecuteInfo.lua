9c05c00abae86d31ecc33699129b819ccab4e5ab
Jodenee <81998397+Jodenee@users.noreply.github.com>
2023-04-12 22:14:18 +0200

    Add new parameters to OptionalExecuteInfo constructor method
    The constructor method can now take a messageFlags object and a string to represent a thread name

--- Discohook/OptionalExecuteInfo.lua
+++ Discohook/OptionalExecuteInfo.lua
@@ -1,13 +1,16 @@
+local MessageFlags = require(script.Parent.MessageFlags)
 local OptionalExecuteInfo = {}
 OptionalExecuteInfo.__index = OptionalExecuteInfo
 
-function OptionalExecuteInfo.new(threadId: string?, tts: boolean?, usernameOverride: string?, avatarOverride: string?)
+function OptionalExecuteInfo.new(threadId: string?, tts: boolean?, usernameOverride: string?, avatarOverride: string?, messageFlags, threadName: string?)
 	local self = setmetatable({}, OptionalExecuteInfo)
 	
-	self.threadId = threadId or ""
+	self.threadId = threadId
 	self.tts = tts or false
 	self.usernameOverride = usernameOverride
 	self.avatarOverride = avatarOverride
+	self.messageFlags = messageFlags or MessageFlags.fromBitfield(0)
+	self.threadName = threadName
 	
 	return self
 end
