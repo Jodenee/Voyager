13ae5a9c29bec356fcdef91941f39ce70abf7e8b
Jodenee <81998397+Jodenee@users.noreply.github.com>
2023-04-12 21:47:24 +0200

    Code clean-up

--- Discohook/Author.lua
+++ Discohook/Author.lua
@@ -8,15 +8,17 @@ function Author.new(data)
 	self.username = data["username"]
 	self.avatar = data["avatar"]
 	self.discriminator = data["discriminator"]
-	self.bot = if data["bot"] ~= nil then data["bot"] else false	
+	self.bot = data["bot"] or false
 	
 	return self
 end
 
 function Author:getAvatarUrl(size: number?, imageFormat: string?): string
-	if imageFormat ~= nil then if imageFormat ~= ".jpeg" and imageFormat ~= ".png" and imageFormat ~= ".webp" and imageFormat ~= ".gif" then return error("Unsupported image format given.") end end
+	if imageFormat then 
+		if imageFormat ~= ".jpeg" and imageFormat ~= ".png" and imageFormat ~= ".webp" and imageFormat ~= ".gif" then return error("Unsupported image format given.") end 
+	end
 	
-	if size ~= nil then
+	if size then
 		if size < 16 or size > 4096 then return error("Image size must be between 16 to 4096.") end
 		if math.sqrt(size) % 1 ~= 0 then return error("Image size must be any power of two") end
 		return "https://cdn.discordapp.com/avatars/" .. self.id .. "/" .. self.avatar .. (imageFormat or ".png") .. "?size=" .. size
