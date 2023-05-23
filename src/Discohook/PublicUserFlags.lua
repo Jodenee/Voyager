4b3d5faf2bff78d154bfcceb702b108984f28cba
Jodenee <81998397+Jodenee@users.noreply.github.com>
2023-05-23 21:30:19 +0200

    Fixed :getFlags not working
    added back PublicUserFlags.fromBitfield

--- src/Discohook/PublicUserFlags.lua
+++ src/Discohook/PublicUserFlags.lua
@@ -3,11 +3,17 @@ local PublicUserFlags = {}
 PublicUserFlags.__index = PublicUserFlags
 setmetatable(PublicUserFlags, BaseFlags)
 
+function PublicUserFlags.fromBitfield(value: number)
+	local self = setmetatable(BaseFlags.fromBitfield(value), PublicUserFlags)
+
+	return self
+end
+
 function PublicUserFlags:getFlags(): {number}
-	local discohookEnum = require(script.Parent).Enum
+	local enum = require(script.Parent.utilities.Enum)
 	local flags = {}
 	
-	for _, flag in discohookEnum.UserPublicFlags do
+	for _, flag in enum.UserPublicFlags do
 		if self:hasFlag(flag) then table.insert(flags, flag) end
 	end
 	
