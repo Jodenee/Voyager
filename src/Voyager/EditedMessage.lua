79c0159a390f7d5279e46d8d607c85e4ef1244d4
Jodenee <81998397+Jodenee@users.noreply.github.com>
2023-12-18 20:30:37 +0100

    Deprecated use of editedTimestamp, use editedAt instead

--- src/Voyager/EditedMessage.lua
+++ src/Voyager/EditedMessage.lua
@@ -9,6 +9,7 @@ function EditedMessage.new(data)
 	local self = setmetatable(Message.new(data), EditedMessage)
 	
 	self.editedTimestamp = data.edited_timestamp
+	self.editedAt = DateTime.fromIsoDate(data.edited_timestamp)
 	
 	return self
 end
