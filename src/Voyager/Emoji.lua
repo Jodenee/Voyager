5866f3cc7219d8e5483f165c99d5970916e22c59
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-03-31 16:37:37 +0200

    Update Emoji
    spacing!

--- src/Voyager/Emoji.lua
+++ src/Voyager/Emoji.lua
@@ -3,11 +3,11 @@ Emoji.__index = Emoji
 
 function Emoji.new(data)
 	local self = setmetatable({}, Emoji)
-	
+
 	self.id = data.id
 	self.name = data.name
 	self.animated = data.animated or false
-	
+
 	return self
 end
 
