58090f8d741ae4d37ce736b2b896c00472a2bea7
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-04-02 21:20:01 +0200

    Update PublicUserFlags
    Delaying release of new enum

--- src/Voyager/PublicUserFlags.lua
+++ src/Voyager/PublicUserFlags.lua
@@ -12,10 +12,10 @@ function PublicUserFlags.fromBitfield(value : number)
 end
 
 function PublicUserFlags:getFlags() : {number}
-	local enum = require(script.Parent.Enum)
+	local enum = require(script.Parent.utilities.Enum)
 	local foundFlags = {}
 	
-	for _, flag in enum.PublicUserFlag do
+	for _, flag in enum.UserPublicFlags do
 		if self:hasFlag(flag) then table.insert(foundFlags, flag) end
 	end
 	
