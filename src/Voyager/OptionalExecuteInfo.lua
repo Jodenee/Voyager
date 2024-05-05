1c9a0c93ae42a8b87f16db5dc4ebe4f956eb193e
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-05-05 15:27:37 +0200

    Update OptionalExecuteInfo.lua
    small typing bug fix

--- src/Voyager/OptionalExecuteInfo.lua
+++ src/Voyager/OptionalExecuteInfo.lua
@@ -3,16 +3,16 @@ OptionalExecuteInfo.__index = OptionalExecuteInfo
 
 local MessageFlags = require(script.Parent.MessageFlags)
 
-function OptionalExecuteInfo.new(threadId : string?, tts : boolean?, usernameOverride : string?, avatarOverride : string?, messageFlags : MessageFlags?, threadName : string?)
+function OptionalExecuteInfo.new(threadId : string?, tts : boolean?, usernameOverride : string?, avatarOverride : string?, messageFlags : {}?, threadName : string?)
 	local self = setmetatable({}, OptionalExecuteInfo)
-	
+
 	self.threadId = threadId
 	self.tts = tts or false
 	self.usernameOverride = usernameOverride
 	self.avatarOverride = avatarOverride
 	self.messageFlags = messageFlags or MessageFlags.fromBitfield(0)
 	self.threadName = threadName
-	
+
 	return self
 end
 
