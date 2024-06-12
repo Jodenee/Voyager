9c027931b7a48b191576b5d0fb0bc10cdc14c414
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-06-12 21:11:16 +0200

    Update Embed
    Embed properties are now in PascalCase. Embed.Color is now a Color3 value, Embed.Timestamp is now a DateTime value.

--- src/Voyager/Embed.lua
+++ src/Voyager/Embed.lua
@@ -3,145 +3,188 @@ Embed.__index = Embed
 
 function Embed.new(title : string?, description : string?, url : string?)
 	local self = setmetatable({}, Embed)
-	
-	self.title = title
-	self.description = description
-	self.url = url
-	
-	self.fields = {}
-	
+
+	self.Title = title
+	self.Description = description
+	self.Url = url
+
+	self.Fields = {}
+
 	return self
 end
 
-function Embed:_validate() : (boolean, string?)
-	if self.title then
-		if string.len(self.title) > 256 then return false, "The title of an embed must only contain up to 256 characters." end 
+function Embed:_Validate() : (boolean, string?)
+	if self.Title then
+		if string.len(self.Title) > 256 then return false, "The title of an embed must only contain up to 256 characters." end
 	end
-	
-	if self.description then
-		if string.len(self.description) > 4096 then return false, "The description of an embed must only contain up to 4096 characters." end 
+
+	if self.Description then
+		if string.len(self.Description) > 4096 then return false, "The description of an embed must only contain up to 4096 characters." end
 	end
-	
-	if self.footer then 
-		if string.len(self.footer.text) > 2048 then return false, "The text in a footer must only contain up to 2048 characters." end 
-		
-		if self.footer.icon_url then
-			if not self.footer.icon_url:match("https") then return false, "The footer's icon url only supports HTTP(S)." end
+
+	if self.Footer then 
+		if string.len(self.Footer.text) > 2048 then return false, "The text in a footer must only contain up to 2048 characters." end
+
+		if self.Footer.icon_url then
+			if not self.Footer.icon_url:match("https") then return false, "The footer's icon url only supports HTTP(S)." end
 		end
 	end
-	
-	if self.image then
-		if not self.image.url:match("https") then return false, "The image's url only supports HTTP(S)." end
+
+	if self.Image then
+		if not self.Image.url:match("https") then return false, "The image's url only supports HTTP(S)." end
 	end
-	
-	if self.thumbnail then
-		if not self.thumbnail.url:match("https") then return false, "The thumbnail's url only supports HTTP(S)." end	
+
+	if self.Thumbnail then
+		if not self.Thumbnail.url:match("https") then return false, "The thumbnail's url only supports HTTP(S)." end
 	end
-	
-	if self.author then 
-		if string.len(self.author.name) > 256 then return false, "The name of an author must only contain up to 256 characters." end
-		
-		if self.author.icon_url then
-			if not self.author.icon_url:match("https") then return false, "The author's icon url only supports HTTP(S)." end
+
+	if self.Author then 
+		if string.len(self.Author.name) > 256 then return false, "The name of an author must only contain up to 256 characters." end
+
+		if self.Author.icon_url then
+			if not self.Author.icon_url:match("https") then return false, "The author's icon url only supports HTTP(S)." end
 		end
 	end
-	
-	if #self.fields > 25 then return false, "One embed must only have up to 25 fields." end
-	
-	for _, field in self.fields do
+
+	if #self.Fields > 25 then return false, "One embed must only have up to 25 fields." end
+
+	for _, field in self.Fields do
 		if string.len(field.name) > 256 then return false, "The name of a field must only contain up to 256 characters." end
-		if string.len(field.value) > 1024 then return false, "The value of a field must only contain up to 1024 characters." end			
+		if string.len(field.value) > 1024 then return false, "The value of a field must only contain up to 1024 characters." end
 	end
-	
+
 	return true
 end
 
-function Embed:setTitle(title : string) : nil	
-	self.title = title
+function Embed:SetTitle(title : string) : nil
+	self.Title = title
 end
 
-function Embed:setDescription(description : string) : nil	
-	self.description = description
+function Embed:SetDescription(description : string) : nil
+	self.Description = description
 end
 
-function Embed:setUrl(url : string) : nil
-	self.url = url
+function Embed:SetUrl(url : string) : nil
+	self.Url = url
 end
 
-function Embed:setTimestamp(customTimestamp : string?) : nil
-	if customTimestamp then self.timestamp = customTimestamp return end
+function Embed:SetTimestamp(customTimestamp : DateTime?) : nil
+	if customTimestamp then self.Timestamp = customTimestamp return end
 
-	self.timestamp = DateTime.now():ToIsoDate()
+	self.Timestamp = DateTime.now()
 end
 
-function Embed:setColor(color3 : Color3) : nil
-	self.color = bit32.lshift((color3.R * 255), 16) + bit32.lshift((color3.G * 255), 8) + (color3.B * 255)
+function Embed:SetColor(color3 : Color3) : nil
+	self.Color = color3
 end
 
-function Embed:setFooter(text : string, iconUrl : string?) : nil
-	self.footer = {
+function Embed:SetFooter(text : string, iconUrl : string?) : nil
+	self.Footer = {
 		text = text,
 		icon_url = iconUrl
 	}
 end
 
-function Embed:setImage(url : string) : nil
-	self.image = {
+function Embed:SetImage(url : string) : nil
+	self.Image = {
 		url = url
 	}
 end
 
-function Embed:setThumbnail(url : string) : nil
+function Embed:SetThumbnail(url : string) : nil
 	self.thumbnail = {
 		url = url
 	}
 end
 
-function Embed:setAuthor(name : string, url : string?, iconUrl : string?) : nil
-	self.author = {
+function Embed:SetAuthor(name : string, url : string?, iconUrl : string?) : nil
+	self.Author = {
 		name = name,
 		url = url,
 		icon_url = iconUrl
 	}
 end
 
-function Embed:addField(name : string, value : string, inLine : boolean?) : nil
-	table.insert(self.fields, {
+function Embed:AddField(name : string, value : string, inLine : boolean?) : nil
+	table.insert(self.Fields, {
 		name = name,
 		value = value,
 		inline = inLine
 	})
 end
 
-function Embed:totalCharacters() : number
+function Embed:TotalCharacters() : number
 	local total = 0
-	
-	if self.title then total += string.len(self.title) end
-	if self.description then total += string.len(self.description) end
-	if self.footer then total += string.len(self.footer.text) end
-	if self.author then total += string.len(self.author.name) end
-	
-	if self.fields then
-		for _, field in self.fields do
+
+	if self.Title then total += string.len(self.Title) end
+	if self.Description then total += string.len(self.Description) end
+	if self.Footer then total += string.len(self.Footer.text) end
+	if self.Author then total += string.len(self.Author.name) end
+
+	if self.Fields then
+		for _, field in self.Fields do
 			total += (string.len(field.name) + string.len(field.value))
 		end
 	end
-	
+
 	return total
 end
 
-function Embed:colorToRGB() : {red : number, green : number, blue : number}?
-	if not self.color then return end
-	
-	local r = bit32.band((bit32.rshift(self.color, (8 * 2))), 0xFF)
-	local g = bit32.band((bit32.rshift(self.color, (8 * 1))), 0xFF)
-	local b = bit32.band((bit32.rshift(self.color, (8 * 0))), 0xFF)
-	
-	return {
-		red = r,
-		green = g,
-		blue = b
+function Embed:_ToObject() : {}
+	local embedObject = {
+		title = self.Title,
+		description = self.Description,
+		url = self.Url
 	}
+
+	if self.Color then
+		embedObject.color = bit32.lshift((self.Color.R * 255), 16) + bit32.lshift((self.Color.G * 255), 8) + (self.Color.B * 255)
+	end
+
+	if self.Timestamp then
+		embedObject.timestamp = self.Timestamp:ToIsoDate()
+	end
+
+	if self.Footer then
+		embedObject.footer = {
+			text = self.Footer.text,
+			icon_url = self.Footer.icon_url
+		}
+	end
+
+	if self.Image then 
+		embedObject.image = {
+			url = self.Image.url
+		}
+	end
+
+	if self.Thumbnail then 
+		embedObject.thumbnail = {
+			url = self.Thumbnail.url
+		}
+	end
+
+	if self.Author then
+		embedObject.author = {
+			name = self.Author.name,
+			url = self.Author.url,
+			icon_url = self.Author.icon_url
+		}
+	end
+
+	if #self.Fields > 0 then
+		embedObject.fields = {}
+
+		for _, field in self.Fields do
+			table.insert(embedObject.fields, {
+				name = field.name,
+				value = field.value,
+				inline = field.inline
+			})
+		end
+	end
+
+	return embedObject
 end
 
 return Embed
