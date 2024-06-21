cc5d9008e7a3d56e49aac8471082d16d65bde8ea
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-06-21 20:47:54 +0200

    Update Webhook
    New stuff + code cleanup

--- src/Voyager/Webhook.lua
+++ src/Voyager/Webhook.lua
@@ -1,11 +1,13 @@
 local Webhook = {}
 Webhook.__index = Webhook
 
+local httpService = game:GetService("HttpService")
+
 local Message = require(script.Parent.Message)
 local EditedMessage = require(script.Parent.EditedMessage)
 local ThreadMessage = require(script.Parent.ThreadMessage)
 local EditedThreadMessage = require(script.Parent.EditedThreadMessage)
-local OptionalExecuteInfo = require(script.Parent.OptionalExecuteInfo)
+local OptionalSendMessageInfo = require(script.Parent.OptionalSendMessageInfo)
 
 export type RatelimitInformation = {
 	Limit : number,
@@ -22,12 +24,19 @@ export type RatelimitedInformation = {
 	RetryAfterSafe : number
 }
 
+export type ProxyInformation = {
+	Proxy: boolean,
+	Message : string?,
+	Reason : string?,
+	Error : string?
+}
+
 export type RequestStatus = {
 	Success : boolean,
 	StatusCode : number,
 	StatusMessage : string,
-	Message : string?,
-	Reason : string?
+	ProxyInformation : ProxyInformation?,
+	RatelimitInformation : (RatelimitInformation | RatelimitedInformation)?
 }
 
 function Webhook.new(id : string, token : string, customProxyUrl : string?)
@@ -40,12 +49,12 @@ function Webhook.new(id : string, token : string, customProxyUrl : string?)
 	return self
 end
 
-function Webhook.FromUrl(webhookUrl: string, customProxyUrl : string?)
+function Webhook.FromUrl(webhookUrl : string, customProxyUrl : string?)
 	local self = setmetatable({}, Webhook)
 	local webhookId, webhookToken = string.match(webhookUrl, "^https://discord.com/api/webhooks/(%d+)/([%w%p]+)$")
 
 	if (not webhookId) or (not webhookToken) then
-		error("Invalid webhook url.")
+		error("Could not parse webhook id and token from given webhook url \"" .. webhookUrl .. "\". Make sure the url is in the following format \"https://discord.com/api/webhooks/123/token\".")
 	end
 
 	self.Id = webhookId
@@ -55,7 +64,7 @@ function Webhook.FromUrl(webhookUrl: string, customProxyUrl : string?)
 	return self
 end
 
-function Webhook:_ValidateExecuteRequest(content : string?, embeds : {}?, optionalExecuteInfo) : (boolean, string?)
+function Webhook:_ValidateSendMessageRequest(content : string?, embeds : {}?, optionalSendMessageInfo : {}?) : (boolean, string?)
 	if (not content or content == "") and (not embeds or #embeds == 0) then
 		return false, "one of content, embeds are required."
 	end
@@ -66,43 +75,47 @@ function Webhook:_ValidateExecuteRequest(content : string?, embeds : {}?, option
 		end
 	end
 
-	if optionalExecuteInfo.usernameOverride then
+	if optionalSendMessageInfo.UsernameOverride then
 		local restrictedUsernames = {"everyone", "here"}
 		local restrictedSubStrings = {"discord", "clyde", "```"}
-		local loweredUsername = optionalExecuteInfo.UsernameOverride:lower()
+		local loweredUsername = optionalSendMessageInfo.UsernameOverride:lower()
 
-		for _, restrictedUsername in restrictedUsernames do	
-			if loweredUsername == restrictedUsername then return false, "Username override is a restricted username. \"" .. restrictedUsername .. "\"" end
+		for _, restrictedUsername in restrictedUsernames do
+			if loweredUsername ~= restrictedUsername then continue end
+
+			return false, "Username override is a restricted username \"" .. restrictedUsername .. "\"."
 		end
 
-		for _, restrictedSubString in restrictedSubStrings do	
-			if loweredUsername:match(restrictedSubString) then return false, "Username override contains a restricted substring. \"" .. restrictedSubString .. "\"" end
+		for _, restrictedSubString in restrictedSubStrings do
+			if not loweredUsername:match(restrictedSubString) then continue end
+
+			return false, "Username override contains a restricted substring \"" .. restrictedSubString .. "\"."
 		end
 
-		if string.len(optionalExecuteInfo.UsernameOverride) < 1 or string.len(optionalExecuteInfo.UsernameOverride) > 80 then
+		if string.len(optionalSendMessageInfo.UsernameOverride) < 1 or string.len(optionalSendMessageInfo.UsernameOverride) > 80 then
 			return false, "Username override must be between 1 and 80 characters in length."
 		end
-	end
-
-	if optionalExecuteInfo.ThreadId and optionalExecuteInfo.ThreadName then
-		return false, "threadId and threadName cannot be used together."
 	end
 
 	if embeds then
 		local totalEmbedCharacters = 0
 
 		if #embeds > 10 then
-			return false, "A single message must only have up to 10 embeds." 
+			return false, "A single message can only have up to 10 embeds."
 		end
 
-		for _, embed in embeds do
+		for index, embed in embeds do
 			local isEmbedValid, errorMessage = embed:_Validate()
-			if not isEmbedValid then return false, errorMessage end
+			if not isEmbedValid then
+				return false, "Embeds[" .. index .. "]: " .. errorMessage
+			end
 
 			totalEmbedCharacters += embed:TotalCharacters()
 		end
 
-		if totalEmbedCharacters > 6000 then return false, "The combined sum of characters across all embeds attached to a message must only be up to 6000 characters." end
+		if totalEmbedCharacters > 6000 then
+			return false, "The combined sum of characters across all embeds attached to a message can only have up to 6000 characters."
+		end
 	end
 
 	return true
@@ -118,52 +131,64 @@ function Webhook:_ValidateEditMessageRequest(content : string?, embeds : {}?) : 
 	if embeds then
 		local totalEmbedCharacters = 0
 
-		for _, embed in embeds do
+		for index, embed in embeds do
 			local isEmbedValid, errorMessage = embed:_Validate()
-			if not isEmbedValid then return false, errorMessage end
+			if not isEmbedValid then
+				return false, "Embeds[" .. index .. "]: " .. errorMessage
+			end
 
 			totalEmbedCharacters += embed:TotalCharacters()
 		end
 
-		if totalEmbedCharacters > 6000 then return false, "The combined sum of characters across all embeds attached to a message must only be up to 6000 characters." end
+		if totalEmbedCharacters > 6000 then
+			return false, "The combined sum of characters across all embeds attached to a message can only have up to 6000 characters."
+		end
 	end
 
 	return true
 end
 
-function Webhook:_Request(url : string, method : string, body : {}?, contentType : string?) : ({}?, RequestStatus, RatelimitInformation | RatelimitedInformation | {})
-	local httpService = game:GetService("HttpService")
-
+function Webhook:_Request(url : string, method : string, body : {}?, contentType : string?) : ({}?, RequestStatus)
 	local response = httpService:RequestAsync({
 		Url = url,
 		Method = method,
-		Headers = {["Content-Type"] = contentType},
+		Headers = {
+			["Content-Type"] = contentType
+		},
 		Body = httpService:JSONEncode(body)
 	})
-	local responseHeaders : {} = response.Headers
-	local decodedBody : {} = {}
-	local ratelimitInformation : RatelimitInformation | RatelimitedInformation | {} = {}
-	local wasRequestQueued : boolean = false
+	local responseHeaders = response.Headers
+	local decodedBody = nil
+	local ratelimitInfo : (RatelimitInformation | RatelimitedInformation)?
+	local proxyInfo : ProxyInformation?
+	local wasRequestQueued = false
 
-	if response.Body ~= "" then
+	if response.Success and response.StatusCode ~= 204 then
 		decodedBody = httpService:JSONDecode(response.Body)
 		wasRequestQueued = decodedBody.proxy
+
+		proxyInfo = {
+			Proxy = decodedBody.proxy,
+			Message = decodedBody.message,
+			Reason = decodedBody.reason,
+			Error = decodedBody.error
+		} :: ProxyInformation
 	end
 
 	if response.Success and not wasRequestQueued then
-		ratelimitInformation = {
+		ratelimitInfo = {
 			Limit = tonumber(responseHeaders["x-ratelimit-limit"]),
 			Remaining = tonumber(responseHeaders["x-ratelimit-remaining"]),
 			Reset = DateTime.fromUnixTimestamp(tonumber(responseHeaders["x-ratelimit-reset"])),
 			ResetAfter = tonumber(responseHeaders["x-ratelimit-reset-after"]),
-			ResetAfterSafe = responseHeaders["x-ratelimit-reset-after"] + 1,
+			ResetAfterSafe = tonumber(responseHeaders["x-ratelimit-reset-after"]) + .5,
 			Bucket = responseHeaders["x-ratelimit-bucket"]
 		} :: RatelimitInformation
 	elseif response.StatusCode == 429 and not wasRequestQueued then
-		ratelimitInformation = {
+		ratelimitInfo = {
 			Scope = responseHeaders["x-ratelimit-scope"] or "global", -- The only time scope is not returned is when a webhook gets cloudflare ratelimited which counts as global.
 			RetryAfter = tonumber(responseHeaders["retry-after"]),
-			RetryAfterSafe = responseHeaders["retry-after"] + 1000
+			RetryAfterSafe = tonumber(responseHeaders["retry-after"]) + 500
 		} :: RatelimitedInformation
 	end
 
@@ -171,32 +196,23 @@ function Webhook:_Request(url : string, method : string, body : {}?, contentType
 		Success = response.Success,
 		StatusCode = response.StatusCode,
 		StatusMessage = response.StatusMessage,
-		Message = decodedBody.message,
-		Reason = decodedBody.reason
+		ProxyInformation = proxyInfo,
+		RatelimitInformation = ratelimitInfo
 	}
 
-	return decodedBody, requestStatus, ratelimitInformation
+	return decodedBody, requestStatus
 end
 
-function Webhook:Execute(content : string?, embeds : {}?, queue : boolean, waitForMessage : boolean, optionalExecuteInfo) : ({}?, RequestStatus, RatelimitInformation | RatelimitedInformation | {})
-	local executeInfo = optionalExecuteInfo or OptionalExecuteInfo.new()
-	local isRequestValid, errorMessage = self:_ValidateExecuteRequest(content, embeds, executeInfo)
+function Webhook:SendMessage(content : string?, embeds : {}?, queue : boolean?, waitForMessage : boolean?, optionalSendMessageInfo : {}?) : ({}?, RequestStatus)
+	optionalSendMessageInfo = optionalSendMessageInfo or OptionalSendMessageInfo.new()
+
+	local isRequestValid, errorMessage = self:_ValidateSendMessageRequest(content, embeds, optionalSendMessageInfo)
 	if not isRequestValid then return error(errorMessage) end
 
 	if queue == nil then queue = true end
 	if waitForMessage == nil then waitForMessage = false end
 
-	local requestUrl = self.BaseUrl
 	local embedObjects = {}
-	local requestBody = {
-		content = content,
-		username = executeInfo.UsernameOverride,
-		avatar_url = executeInfo.AvatarOverride,
-		tts = executeInfo.TTS,
-		embeds = embedObjects,
-		flags = executeInfo.MessageFlags.Value,
-		thread_name = executeInfo.ThreadName
-	}
 
 	if embeds then
 		for _, embed in embeds do
@@ -204,61 +220,141 @@ function Webhook:Execute(content : string?, embeds : {}?, queue : boolean, waitF
 		end
 	end
 
-	if queue then requestUrl ..= "/queue" end
-	requestUrl ..= "?wait=" .. tostring(waitForMessage)
-	if executeInfo.ThreadId then requestUrl ..= "&thread_id=" .. executeInfo.ThreadId end
+	local requestUrl = self.BaseUrl
+	local requestBody = {
+		content = content,
+		username = optionalSendMessageInfo.UsernameOverride,
+		avatar_url = optionalSendMessageInfo.AvatarOverride,
+		tts = optionalSendMessageInfo.UseTTS,
+		embeds = embedObjects,
+		flags = optionalSendMessageInfo.MessageFlags.Value,
+		thread_name = optionalSendMessageInfo.ThreadName
+	}
 
-	local responseBody, requestStatus, requestRatelimitInfo = self:_Request(requestUrl, "POST", requestBody, "application/json")
+	if queue then
+		requestUrl ..= "/queue"
+	end
+
+	requestUrl ..= "?wait=" .. tostring(waitForMessage)
+
+	local responseBody, requestStatus = self:_Request(requestUrl, "POST", requestBody, "application/json")
 
 	if not queue and waitForMessage and requestStatus.Success then
-		if not executeInfo.threadId then
-			return Message.new(responseBody), requestStatus, requestRatelimitInfo
-		else
-			return ThreadMessage.new(responseBody), requestStatus, requestRatelimitInfo
+		return Message.new(responseBody), requestStatus
+	end
+
+	return nil, requestStatus
+end
+
+function Webhook:SendMessageInThread(threadId : string, content : string?, embeds : {}?, queue : boolean?, waitForMessage : boolean?, optionalSendMessageInfo : {}?) : ({}?, RequestStatus)
+	optionalSendMessageInfo = optionalSendMessageInfo or OptionalSendMessageInfo.new()
+
+	local isRequestValid, errorMessage = self:_ValidateSendMessageRequest(content, embeds, optionalSendMessageInfo)
+	if not isRequestValid then return error(errorMessage) end
+
+	if queue == nil then queue = true end
+	if waitForMessage == nil then waitForMessage = false end
+
+	local embedObjects = {}
+
+	if embeds then
+		for _, embed in embeds do
+			table.insert(embedObjects, embed:_ToObject())
 		end
 	end
 
-	return nil, requestStatus, requestRatelimitInfo
+	local requestUrl = self.BaseUrl
+	local requestBody = {
+		content = content,
+		username = optionalSendMessageInfo.UsernameOverride,
+		avatar_url = optionalSendMessageInfo.AvatarOverride,
+		tts = optionalSendMessageInfo.UseTTS,
+		embeds = embedObjects,
+		flags = optionalSendMessageInfo.MessageFlags.Value
+	}
+
+	if queue then
+		requestUrl ..= "/queue"
+	end
+
+	requestUrl ..= "?wait=" .. tostring(waitForMessage)
+	requestUrl ..= "&thread_id=" .. threadId
+
+	local responseBody, requestStatus = self:_Request(requestUrl, "POST", requestBody, "application/json")
+
+	if not queue and waitForMessage and requestStatus.Success then
+		return ThreadMessage.new(responseBody), requestStatus
+	end
+
+	return nil, requestStatus
 end
 
-function Webhook:EditMessage(messageId : string, content : string?, embeds : {}?, threadId : string?) : ({}?, RequestStatus, RatelimitInformation | RatelimitedInformation)
+function Webhook:EditMessage(messageId : string, content : string?, embeds : {}?, queue : boolean?) : ({}?, RequestStatus)
 	local isRequestValid, errorMessage = self:_ValidateEditMessageRequest(content, embeds)
 	if not isRequestValid then return error(errorMessage) end
 
-	local requestUrl = self.BaseUrl .. "/messages/" .. messageId
 	local embedObjects = {}
+
+	if embeds then
+		for _, embed in embeds do
+			table.insert(embedObjects, embed:_ToObject())
+		end
+	end
+
+	local requestUrl = self.BaseUrl .. "/messages/" .. messageId
 	local requestBody = {
 		content = content,
 		embeds = embedObjects
 	}
 
+	local responseBody, requestStatus = self:_Request(requestUrl, "PATCH", requestBody, "application/json")
+
+	if requestStatus.Success then
+		return EditedMessage.new(responseBody), requestStatus
+	end
+
+	return nil, requestStatus
+end
+
+function Webhook:EditMessageInThread(threadId : string, messageId : string, content : string?, embeds : {}?) : ({}?, RequestStatus)
+	local isRequestValid, errorMessage = self:_ValidateEditMessageRequest(content, embeds)
+	if not isRequestValid then return error(errorMessage) end
+
+	local embedObjects = {}
+
 	if embeds then
 		for _, embed in embeds do
 			table.insert(embedObjects, embed:_ToObject())
 		end
 	end
 
-	if threadId then requestUrl ..= "?thread_id=" .. threadId end
+	local requestUrl = self.BaseUrl .. "/messages/" .. messageId .. "?thread_id=" .. threadId
+	local requestBody = {
+		content = content,
+		embeds = embedObjects
+	}
 
-	local responseBody, requestStatus, requestRatelimitInfo = self:_Request(requestUrl, "PATCH", requestBody, "application/json")
+	local responseBody, requestStatus = self:_Request(requestUrl, "PATCH", requestBody, "application/json")
 
-	if not requestStatus.Success then return nil, requestStatus end
-
-	if not threadId then
-		return EditedMessage.new(responseBody), requestStatus, requestRatelimitInfo
-	else
-		return EditedThreadMessage.new(responseBody), requestStatus, requestRatelimitInfo
+	if requestStatus.Success then
+		return EditedThreadMessage.new(responseBody), requestStatus
 	end
+
+	return nil, requestStatus
 end
 
-function Webhook:DeleteMessage(messageId : string, threadId : string?) : (RequestStatus, RatelimitInformation | RatelimitedInformation)
+function Webhook:DeleteMessage(messageId : string) : RequestStatus
 	local requestUrl = self.BaseUrl .. "/messages/" .. messageId
+	local _, requestStatus = self:_Request(requestUrl, "DELETE")
 
-	if threadId then requestUrl ..= "?thread_id=" .. threadId end
+	return requestStatus
+end
 
-	local _, requestStatus, requestRatelimitInfo = self:_Request(requestUrl, "DELETE")
+function Webhook:DeleteMessageInThread(threadId : string, messageId : string) : RequestStatus
+	local requestUrl = self.BaseUrl .. "/messages/" .. messageId .. "?thread_id=" .. threadId
+	local _, requestStatus = self:_Request(requestUrl, "DELETE")
 
-	return requestStatus, requestRatelimitInfo
+	return requestStatus
 end
 
 return Webhook
