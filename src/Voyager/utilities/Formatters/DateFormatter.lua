fc644525472235d952939e8a4483b9793aebf895
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-06-21 20:39:02 +0200

    Update Formatter
    Removed semi-colons
    
    Sorry, i did too much C#

--- src/Voyager/utilities/Formatters/DateFormatter.lua
+++ src/Voyager/utilities/Formatters/DateFormatter.lua
@@ -1,31 +1,31 @@
 local DateFormatter = {}
 
 function DateFormatter.RelativeTime(datetime : DateTime) : string
-	return "<t:" .. datetime.UnixTimestamp .. ":R>";
+	return "<t:" .. datetime.UnixTimestamp .. ":R>"
 end
 
 function DateFormatter.LongDate(datetime : DateTime) : string
-	return "<t:" .. datetime.UnixTimestamp .. ":D>";
+	return "<t:" .. datetime.UnixTimestamp .. ":D>"
 end
 
 function DateFormatter.ShortDate(datetime : DateTime) : string
-	return "<t:" .. datetime.UnixTimestamp .. ":d>";
+	return "<t:" .. datetime.UnixTimestamp .. ":d>"
 end
 
 function DateFormatter.LongTime(datetime : DateTime) : string
-	return "<t:" .. datetime.UnixTimestamp .. ":T>";
+	return "<t:" .. datetime.UnixTimestamp .. ":T>"
 end
 
 function DateFormatter.ShortTime(datetime : DateTime) : string
-	return "<t:" .. datetime.UnixTimestamp .. ":t>";
+	return "<t:" .. datetime.UnixTimestamp .. ":t>"
 end
 
 function DateFormatter.LongDateTime(datetime : DateTime) : string
-	return "<t:" .. datetime.UnixTimestamp .. ":F>";
+	return "<t:" .. datetime.UnixTimestamp .. ":F>"
 end
 
 function DateFormatter.ShortDateTime(datetime : DateTime) : string
-	return "<t:" .. datetime.UnixTimestamp .. ":f>";
+	return "<t:" .. datetime.UnixTimestamp .. ":f>"
 end
 
 return DateFormatter
