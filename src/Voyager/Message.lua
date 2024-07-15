e27e794bdf8932552a938aa18dd036493934a36c
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-07-15 16:44:11 +0200

    Update Message.lua
    Moved embed construction to Embed._FromObject. Turns out we can just use the raw mention_roles for the MentionedRoles property

--- src/Voyager/Message.lua
+++ src/Voyager/Message.lua
@@ -4,11 +4,6 @@ Message.__index = Message
 local User = require(script.Parent.User)
 local Embed = require(script.Parent.Embed)
 local MessageFlags = require(script.Parent.MessageFlags)
-local EmbedFooter = require(script.Parent.EmbedFooter)
-local EmbedImage = require(script.Parent.EmbedImage)
-local EmbedThumbnail = require(script.Parent.EmbedThumbnail)
-local EmbedAuthor = require(script.Parent.EmbedAuthor)
-local EmbedField = require(script.Parent.EmbedField)
 
 function Message.new(data)
 	local self = setmetatable({}, Message)
@@ -20,7 +15,7 @@ function Message.new(data)
 	self.Author = User.new(data.author)
 	self.Embeds = {}
 	self.MentionedUsers = {}
-	self.MentionedRoles = {}
+	self.MentionedRoles = data.mention_roles
 	self.IsPinned = data.pinned
 	self.MentionsEveryone = data.mention_everyone
 	self.IsTTS = data.tts
@@ -36,55 +31,13 @@ function Message.new(data)
 
 	if data.embeds then
 		for _, embedObject in data.embeds do
-			local embed = Embed.new(embedObject.title, embedObject.description, embedObject.url)
-
-			if embedObject.color then
-				local red = bit32.band(bit32.rshift(embedObject.color, 16), 255)
-				local green = bit32.band(bit32.rshift(embedObject.color, 8), 255)
-				local blue = bit32.band(embedObject.color, 255)
-
-				embed:SetColor(Color3.fromRGB(red, green, blue))
-			end
-
-			if embedObject.timestamp then
-				embed:SetTimestamp(embedObject.timestamp)
-			end
-
-			if embedObject.footer then
-				embed.Footer = EmbedFooter._FromObject(embedObject.footer)
-			end
-
-			if embedObject.image then
-				embed.Image = EmbedImage._FromObject(embedObject.image)
-			end
-
-			if embedObject.thumbnail then
-				embed.Thumbnail = EmbedThumbnail._FromObject(embedObject.thumbnail)
-			end
-
-			if embedObject.author then
-				embed.Author = EmbedAuthor._FromObject(embedObject.author)
-			end
-
-			if embedObject.fields then
-				for _, fieldObject in embedObject.fields do
-					table.insert(embed.Fields, EmbedField._FromObject(fieldObject))
-				end
-			end
-
-			table.insert(self.Embeds, embed)
+			table.insert(self.Embeds, Embed._FromObject(embedObject))
 		end
 	end
 
 	if data.mentions then
 		for _, mentionData in data.mentions do
 			table.insert(self.MentionedUsers, User.new(mentionData))
-		end
-	end
-
-	if data.mention_roles then
-		for _, roleId in data.mention_roles do
-			table.insert(self.MentionedRoles, roleId)
 		end
 	end
 
