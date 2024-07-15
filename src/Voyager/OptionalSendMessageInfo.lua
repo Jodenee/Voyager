b83d79412e4699ebb8b110d533076d8b335041f0
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-07-15 16:47:05 +0200

    Update OptionalSendMessageInfo.lua
    No longer creates a defualt MessageFlags object, nor sets UseTTS to false upon initialisation

--- src/Voyager/OptionalSendMessageInfo.lua
+++ src/Voyager/OptionalSendMessageInfo.lua
@@ -1,15 +1,13 @@
 local OptionalSendMessageInfo = {}
 OptionalSendMessageInfo.__index = OptionalSendMessageInfo
 
-local MessageFlags = require(script.Parent.MessageFlags)
-
-function OptionalSendMessageInfo.new(useTTS : boolean?, usernameOverride : string?, avatarOverride : string?, messageFlags : {}?, threadName : string?)
+function OptionalSendMessageInfo.new(useTTS : boolean?, usernameOverride : string?, avatarOverride : string?, flags : {}?, threadName : string?)
 	local self = setmetatable({}, OptionalSendMessageInfo)
 
-	self.UseTTS = useTTS or false
+	self.UseTTS = useTTS
 	self.UsernameOverride = usernameOverride
 	self.AvatarOverride = avatarOverride
-	self.MessageFlags = messageFlags or MessageFlags.FromBitfield(0)
+	self.Flags = flags
 	self.ThreadName = threadName
 
 	return self
