60c18570cfbe512a05066e7faebbf1e0cb14cd17
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-07-15 16:51:12 +0200

    Update User.lua
    Renamed the property Avatar to AvatarHash

--- src/Voyager/User.lua
+++ src/Voyager/User.lua
@@ -9,7 +9,7 @@ function User.new(data)
 
 	self.Id = data.id
 	self.Username = data.username
-	self.Avatar = data.avatar
+	self.AvatarHash = data.avatar
 	self.IsBot = data.bot or false
 	self.Mention = MentionFormatter.MentionUser(data.id)
 	self.GlobalName = data.global_name
@@ -19,11 +19,11 @@ function User.new(data)
 end
 
 function User:GetAvatarUrl(size : number?, imageFormat : (".jpeg" | ".png" | ".webp" | ".gif")?) : string?
-	if not self.Avatar then return end
+	if not self.AvatarHash then return end
 
 	if imageFormat then
 		if imageFormat ~= ".jpeg" and imageFormat ~= ".png" and imageFormat ~= ".webp" and imageFormat ~= ".gif" then
-			return error("Unsupported image format given.") 
+			return error("Unsupported image format given.")
 		end
 	end
 
@@ -31,16 +31,16 @@ function User:GetAvatarUrl(size : number?, imageFormat : (".jpeg" | ".png" | ".w
 		if size < 16 or size > 4096 then return error("Image size must be between 16 and 4096.") end
 		if math.sqrt(size) % 1 ~= 0 then return error("Image size must be any number to power of two.") end
 
-		return "https://cdn.discordapp.com/avatars/" .. self.Id .. "/" .. self.Avatar .. (imageFormat or ".png") .. "?size=" .. size
+		return "https://cdn.discordapp.com/avatars/" .. self.Id .. "/" .. self.AvatarHash .. (imageFormat or ".png") .. "?size=" .. size
 	else
-		return "https://cdn.discordapp.com/avatars/" .. self.Id .. "/" .. self.Avatar .. (imageFormat or ".png")
+		return "https://cdn.discordapp.com/avatars/" .. self.Id .. "/" .. self.AvatarHash .. (imageFormat or ".png")
 	end
 end
 
 function User:HasAnimatedAvatar() : boolean
-	if not self.Avatar then return false end
+	if not self.AvatarHash then return false end
 
-	return self.Avatar:match("a_") == "a_"
+	return self.AvatarHash:match("a_") == "a_"
 end
 
 return User
