60b17803aa9edfb8cb4c01c529734640a5f5076a
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-07-19 18:32:39 +0200

    Add init.lua

---
+++ src/init.lua
@@ -0,0 +1,33 @@
+--[[
+	Packages up all Voyager classes in one table and exposes them publicly.
+]]
+
+local voyager = {
+    Bases = {
+        BaseFlags = require(script.Bases.BaseFlags)
+    },
+
+    Utilities = {
+        Formatters = {
+            DateFormatters = require(script.Utilities.Formatters.DateFormatter),
+            EmojiFormatter = require(script.Utilities.Formatters.EmojiFormatter),
+            MentionFormatter = require(script.Utilities.Formatters.MentionFormatter)
+        }
+    },
+
+    Embed = require(script.Embed),
+    EmbedAuthor = require(script.EmbedAuthor),
+    EmbedField = require(script.EmbedField),
+    EmbedFooter = require(script.EmbedFooter),
+    EmbedImage = require(script.EmbedImage),
+    EmbedThumbnail = require(script.EmbedThumbnail),
+    Enum = require(script.Enum),
+    Message = require(script.Message),
+    MessageFlags = require(script.MessageFlags),
+    OptionalSendMessageInfo = require(script.OptionalSendMessageInfo),
+    PublicUserFlags = require(script.PublicUserFlags),
+    User = require(script.User),
+    Webhook = require(script.Webhook)
+}
+
+return voyager
